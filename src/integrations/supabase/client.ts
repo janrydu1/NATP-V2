e42273060a99424d594a763576b6db37a6b83941
Muichiro Tokito <sainathrpoojary@gmail.com>
2025-05-29 16:26:22 +0530

    feat(env): Add environment variables for Supabase configuration

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,10 +2,12 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://koljfqumuugekkvmupcx.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtvbGpmcXVtdXVnZWtrdm11cGN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU2NDUxNTIsImV4cCI6MjA2MTIyMTE1Mn0.q_Ti5BMmR2LXbXDshiDIVzI6Ke7A7XIIEcrrj0AxdqI";
+console.log(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY);
+
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
-export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
