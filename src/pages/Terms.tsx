d8d4d5c896ea5551ce3ffb8b692761e27d7b8765
Hritik Sharma <hritiksharma2004@gmail.com>
2025-10-01 00:10:10 +0530

    refactor: Update Terms and Conditions content for clarity and consistency

--- src/pages/Terms.tsx
+++ src/pages/Terms.tsx
@@ -7,50 +7,50 @@ export default function Terms() {
     {
       title: "INTRODUCTION",
       content:
-        "Welcome! These Terms and Conditions (“T&C”) explain the agreement between ATP Service Inc (the Provider, contact: info@atp-trademark.com) and you (the Customer) when you use or purchase our Prepaid Service. By using our service, you agree to these terms. If you’d like a printable copy, just ask us.",
+        "Welcome! These Terms and Conditions (\"T&C\") explain the agreement between ATP Service Inc (the Provider, contact: info@atp-trademark.com) and you (the Customer) when you use or purchase our Prepaid Service. By using our service, you agree to these terms. If you'd like a printable copy, just ask us.",
     },
     {
       title: "DEFINITIONS",
       content:
-        "1.1 Database: Our proprietary system that gathers data from various trademark registers, including national, EU, and international sources.</br>1.2 Prepaid Service: Paid access to secure sections of our Database on atp-trademark.com for three years, starting once we receive your payment.</br>1.3 Service Fee: The upfront cost for accessing the Prepaid Service, as listed in our current offer.</br>1.4 Agreement: The contract between you and us, activated when we receive your Service Fee.",
+        "2.1 Database: Our proprietary system that gathers data from various trademark registers, including national, EU, and international sources.</br>2.2 Prepaid Service: Paid access to secure sections of our Database on atp-trademark.com for three years, starting once we receive your payment.</br>2.3 Service Fee: The upfront cost for accessing the Prepaid Service, as listed in our current offer.</br>2.4 Agreement: The contract between you and us, activated when we receive your Service Fee.",
     },
     {
       title: "SERVICE DESCRIPTION",
-      content: `2.1 Scope: </br><p style="padding-left: 10px;">• You get access to premium content in our Database. </p><p style="padding-left: 10px;">• Service lasts for three years after payment. </p>2.2 Exclusivity: <p style="padding-left: 10px;">• Only these T&C apply to our agreement. </p><p style="padding-left: 10px;">• Other terms from you or third parties don’t apply. </p>2.3 Modifications: <p style="padding-left: 10px;">• We may update these T&C, but we’ll notify you in writing first. </p><p style="padding-left: 10px;">• Changes take effect once we’ve let you know and you acknowledge them.</p> `,
+      content: `3.1 Scope: </br><p style="padding-left: 10px;">• You get access to premium content in our Database. </p><p style="padding-left: 10px;">• Service lasts for three years after payment. </p>3.2 Exclusivity: <p style="padding-left: 10px;">• Only these T&C apply to our agreement. </p><p style="padding-left: 10px;">• Other terms from you or third parties don't apply. </p>3.3 Modifications: <p style="padding-left: 10px;">• We may update these T&C, but we'll notify you in writing first. </p><p style="padding-left: 10px;">• Changes take effect once we've let you know and you acknowledge them.</p> `,
     },
     {
       title: "PAYMENT CONDITIONS",
       content:
-        "3.1 You’ll get access to the service only after you’ve paid the Service Fee in full.</br>3.2 Please pay by bank transfer or check to our specified account. Your access starts once we receive your payment.",
+        "4.1 You'll get access to the service only after you've paid the Service Fee in full.</br>4.2 Please pay by bank transfer or check to our specified account. Your access starts once we receive your payment.",
     },
     {
       title: "RIGHT OF WITHDRAWAL",
-      content: `4.1 Contract Formation: </br><p style="padding-left: 10px;">• The agreement is binding once you pay the Service Fee in response to our offer. </p>4.2 Withdrawal Period: <p style="padding-left: 10px;">• You can cancel within seven days of payment, no questions asked. </p><p style="padding-left: 10px;">• The agreement starts as soon as you pay. </p>`,
+      content: `5.1 Contract Formation: </br><p style="padding-left: 10px;">• The agreement is binding once you pay the Service Fee in response to our offer. </p>5.2 Withdrawal Period: <p style="padding-left: 10px;">• You can cancel within seven days of payment, no questions asked. </p><p style="padding-left: 10px;">• The agreement starts as soon as you pay. </p>`,
     },
     {
       title: "LIABILITY AND DISCLAIMERS",
-      content: `5.1 Data Integrity: </br><p style="padding-left: 10px;">• Our Database is compiled from third-party sources and provided “as is.” We can’t guarantee it’s always accurate or complete. </p>5.2 Limitation of Liability: </br><p style="padding-left: 10px;">• We’re not responsible for any direct or indirect damages, lost revenue, missed opportunities, or service interruptions from using or not using atp-trademark.com. </p><p style="padding-left: 10px;">• This applies even if we knew about possible issues. </p>`,
+      content: `6.1 Data Integrity: </br><p style="padding-left: 10px;">• Our Database is compiled from third-party sources and provided "as is." We can't guarantee it's always accurate or complete. </p>6.2 Limitation of Liability: </br><p style="padding-left: 10px;">• We're not responsible for any direct or indirect damages, lost revenue, missed opportunities, or service interruptions from using or not using atp-trademark.com. </p><p style="padding-left: 10px;">• This applies even if we knew about possible issues. </p>`,
     },
     {
       title: "ARTICLE PUBLICATION SERVICE",
-      content: `6.1 Overview: </br><p style="padding-left: 10px;">• After payment, we may offer to publish an article highlighting your brand’s achievements. </p>6.2 Purpose: </br><p style="padding-left: 10px;">• This helps showcase and strengthen your brand’s reputation. </p>6.3 Editorial Rights: </br><p style="padding-left: 10px;">• We may edit submitted content for clarity and consistency, but won’t change the facts. </p>6.4 Disclaimer: </br><p style="padding-left: 10px;">• We’re not liable for any damages related to this optional service. </p>`,
+      content: `7.1 Overview: </br><p style="padding-left: 10px;">• After payment, we may offer to publish an article highlighting your brand's achievements. </p>7.2 Purpose: </br><p style="padding-left: 10px;">• This helps showcase and strengthen your brand's reputation. </p>7.3 Editorial Rights: </br><p style="padding-left: 10px;">• We may edit submitted content for clarity and consistency, but won't change the facts. </p>7.4 Disclaimer: </br><p style="padding-left: 10px;">• We're not liable for any damages related to this optional service. </p>`,
     },
     {
       title: "DURATION AND TERMINATION",
-      content: `7.1 Contract Period: </br><p style="padding-left: 10px;">• The agreement ends automatically after one year and can’t be terminated early for ordinary reasons. </p>7.2 Termination for Cause: </br><p style="padding-left: 10px;">• Either party can end the agreement immediately if there’s a serious breach. </p><p style="padding-left: 10px;">• The breaching party should be given a chance to fix issues within a reasonable time. </p><p style="padding-left: 10px;">• If it’s unreasonable to continue, immediate termination is allowed. </p>`,
+      content: `8.1 Contract Period: </br><p style="padding-left: 10px;">• The agreement ends automatically after one year and can't be terminated early for ordinary reasons. </p>8.2 Termination for Cause: </br><p style="padding-left: 10px;">• Either party can end the agreement immediately if there's a serious breach. </p><p style="padding-left: 10px;">• The breaching party should be given a chance to fix issues within a reasonable time. </p><p style="padding-left: 10px;">• If it's unreasonable to continue, immediate termination is allowed. </p>`,
     },
     {
       title: "REFUND POLICY",
       content:
-        "8.1 If you’re not satisfied, you can request a refund within 30 days of payment. Email info@atp-trademark.com with your order details and reason. Refunds follow these T&C and any additional policies. This policy applies only to the services described here.",
+        "9.1 If you're not satisfied, you can request a refund within 30 days of payment. Email info@atp-trademark.com with your order details and reason. Refunds follow these T&C and any additional policies. This policy applies only to the services described here.",
     },
     {
       title: "GOVERNING LAW AND DISPUTE SETTLEMENT",
-      content: `9.1 Jurisdiction: </br><p style="padding-left: 10px;">• Our agreement and these T&C are governed by United States law. </p>9.2 Dispute Resolution: </br><p style="padding-left: 10px;">• We’ll try to resolve any issues amicably. </p><p style="padding-left: 10px;">• If that doesn’t work, legal action can be taken in a competent court. </p>`,
+      content: `10.1 Jurisdiction: </br><p style="padding-left: 10px;">• Our agreement and these T&C are governed by United States law. </p>10.2 Dispute Resolution: </br><p style="padding-left: 10px;">• We'll try to resolve any issues amicably. </p><p style="padding-left: 10px;">• If that doesn't work, legal action can be taken in a competent court. </p>`,
     },
     {
       title: "DATA PRIVACY",
-      content: `10.1 Data Handling: </br><p style="padding-left: 10px;">• Details about how we handle your personal data are in our Privacy Policy. </p>10.2 Policy Access:</br><p style="padding-left: 10px;">• You’ll find a link to our Privacy Policy on atp-trademark.com. Please review it to learn how your data is used. </p>`,
+      content: `11.1 Data Handling: </br><p style="padding-left: 10px;">• Details about how we handle your personal data are in our Privacy Policy. </p>11.2 Policy Access:</br><p style="padding-left: 10px;">• You'll find a link to our Privacy Policy on atp-trademark.com. Please review it to learn how your data is used. </p>`,
     },
   ];
 
